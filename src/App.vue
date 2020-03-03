496e735ff58cf694981f875746b5cf65bb5ad20e
Piter <to20099@gmail.com>
2020-03-03 22:24:17 +0200

    Added views components and routs for them.

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+
+    <component :is="layout">
+      <router-view/>
+    </component>
+
+    <!-- <emptyLayout></emptyLayout> -->
+    <!-- <mainLayout></mainLayout>  -->
+
+  </div>
+</template>
+
+<script>
+import emptyLayout from '@/layouts/empty.layout'
+import mainLayout from '@/layouts/main.layout'
+
+export default {
+  computed: {
+    layout() {
+      console.log(this.$route.meta);
+      return (this.$route.meta.layout || 'empty') + '-layout';
+    }
+  },
+  components: {
+    emptyLayout, mainLayout
+  }
+}
+</script>
+
+<style lang="scss">
+@import "~materialize-css/dist/css/materialize.min.css";
+@import "assets/index.css";
+</style>
